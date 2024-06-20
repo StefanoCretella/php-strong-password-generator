04a025c21145c89a40f3f016156cb4e4b6f18e49
StefanoCretella <cretellastefano213@gmail.com>
2024-06-20 19:30:30 +0200

    [UPD] Milestone 4, Added advanced options to generate custom passwords: choice of characters (numbers, letters, symbols) and repetition management

--- functions.php
+++ functions.php
@@ -1,11 +1,35 @@
 <?php
 // Funzione per generare una password casuale di lunghezza specificata
-function generatePassword($length) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
-    $password = '';
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $characters[rand(0, strlen($characters) - 1)];
+function generatePassword($length, $include_numbers = true, $include_letters = true, $include_symbols = true, $no_repeat = false) {
+    $characters = '';
+    
+    if ($include_numbers) {
+        $characters .= '0123456789';
     }
+    if ($include_letters) {
+        $characters .= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if ($include_symbols) {
+        $characters .= '!@#$%^&*()';
+    }
+    
+    $password = '';
+    $characters_length = strlen($characters);
+    $used_characters = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        if ($no_repeat) {
+            $random_char = $characters[rand(0, $characters_length - 1)];
+            while (strpos($used_characters, $random_char) !== false) {
+                $random_char = $characters[rand(0, $characters_length - 1)];
+            }
+            $used_characters .= $random_char;
+            $password .= $random_char;
+        } else {
+            $password .= $characters[rand(0, $characters_length - 1)];
+        }
+    }
+
     return $password;
 }
 ?>
--- index.php
+++ index.php
@@ -11,8 +11,31 @@
 
     <!-- Form per inserire la lunghezza della password -->
     <form action="index.php" method="get">
+
+        <!-- Include lunghezza password (1 a 20 caratteri) -->
         <label for="length">Lunghezza Password:</label>
         <input type="number" id="length" name="length" min="1" max="20" required>
+        <br>
+
+        <!-- Checkbox numeri -->
+        <input type="checkbox" id="numbers" name="numbers" value="true">
+        <label for="numbers">Includi numeri</label>
+        <br>
+
+        <!-- Checkbox lettere -->
+        <input type="checkbox" id="letters" name="letters" value="true">
+        <label for="letters">Includi lettere</label>
+        <br>
+
+        <!-- Checkbox simboli -->
+        <input type="checkbox" id="symbols" name="symbols" value="true">
+        <label for="symbols">Includi simboli</label>
+        <br>
+
+        <!-- Checkbox no_repeat -->
+        <input type="checkbox" id="no_repeat" name="no_repeat" value="true">
+        <label for="no_repeat">Evita ripetizioni di caratteri</label>
+        <br>
         <button type="submit">Genera</button>
     </form>
 
@@ -22,15 +45,20 @@
     // Verifica se è stata inviata la lunghezza della password tramite GET
     if (isset($_GET['length'])) {
         $length = intval($_GET['length']);
+        $include_numbers = isset($_GET['numbers']);
+        $include_letters = isset($_GET['letters']);
+        $include_symbols = isset($_GET['symbols']);
+        $no_repeat = isset($_GET['no_repeat']);
 
-        // Controlla se la lunghezza è valida (da 1 a 20 caratteri)
-        if ($length > 0 && $length <= 20) {
-
+        // Controlla se almeno una opzione è stata selezionata
+        if (!$include_numbers && !$include_letters && !$include_symbols) {
+            echo '<p>Seleziona almeno un tipo di carattere.</p>';
+        } else if ($length > 0 && $length <= 20) {
             // Includi il file functions.php
             include 'functions.php';
 
-            // Genera la password
-            $password = generatePassword($length);
+            // Genera la password con le opzioni selezionate
+            $password = generatePassword($length, $include_numbers, $include_letters, $include_symbols, $no_repeat);
 
             // Salva la password nella sessione
             $_SESSION['generated_password'] = $password;
